60fda9cb6dfbbd327b1a2c9964c9cab8e5a1e310
Speldosa <martin.asperholm@gmail.com>
2022-08-09 20:14:15 +0100

    .

--- DMXmidiator.py
+++ DMXmidiator.py
@@ -376,9 +376,13 @@ with DMXInterface("FT232R") as interface:
                                     for Count in range(len(Layer1.Array_of_Layer1_objects)):
                                         Tmp = Count/(len(Layer1.Array_of_Layer1_objects) - 1)
                                         Layer1.Array_of_Layer1_objects[Count] = Layer1_light_object(
+                                            # Hue = Signal(ADSR(After_attack_amplitude=1, After_decay_amplitude=1, Attack=Tmp, Decay=0, Sustain=2, Release=0), LFO()),
+                                            # Saturation = Signal(ADSR(After_attack_amplitude=0, After_decay_amplitude=1, Attack=Tmp, Decay=0, Sustain=1, Release=0), LFO()),
+                                            # Brightness = Signal(ADSR(After_attack_amplitude=0, After_decay_amplitude=0.5, Attack=Tmp, Decay=0, Sustain=1, Release=0), LFO(Waveform = "Sine", Amplitude = 1, Repeat = True, Rate = 0, Phase = -0.25 - Tmp))
                                             Hue = Signal(ADSR(After_attack_amplitude=1, After_decay_amplitude=1, Attack=Tmp, Decay=0, Sustain=2, Release=0), LFO()),
-                                            Saturation = Signal(ADSR(After_attack_amplitude=0, After_decay_amplitude=1, Attack=Tmp, Decay=0, Sustain=2, Release=0), LFO(Waveform = "Sine", Amplitude = 1, Repeat = True, Rate = 0, Phase = -0.25 - Tmp)),
-                                            Brightness = Signal(ADSR(After_attack_amplitude=0, After_decay_amplitude=1, Attack=Tmp, Decay=0, Sustain=2, Release=0), LFO(Waveform = "Sine", Amplitude = 1, Repeat = True, Rate = 0, Phase = -0.25 - Tmp))
+                                            Saturation = Signal(ADSR(After_attack_amplitude=0, After_decay_amplitude=1, Attack=Tmp, Decay=0, Sustain=1, Release=0), LFO()),
+                                            Brightness = Signal(ADSR(After_attack_amplitude=0, After_decay_amplitude=0.5, Attack=Tmp, Decay=0, Sustain=1, Release=0), LFO(Waveform = "Sine", Amplitude = 1, Repeat = True, Rate = 0, Phase = -0.25 - Tmp))
+                                            # CC_to_ratio(Layer2.Parameters[3][0])
                                         )
 
                             Layer2.Program[1] = Layer2.Program[0] # Finally, copy the program to be implemented to program that is currently running...
