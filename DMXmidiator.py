9aee41494714f1bf5fe682e8974dfa7c1809c873
Speldosa <martin.asperholm@ki.se>
2022-08-10 02:17:43 +0200

    .

--- DMXmidiator.py
+++ DMXmidiator.py
@@ -15,6 +15,7 @@ Number_of_lights = 32 # How many individually controllable lights you want to co
 Midi_device = 'Elektron Syntakt:Elektron Syntakt MIDI 1' # Change this variable to whatever device you want to control the program. In order to get a list of all available devices, you can run: print(mido.get_input_names()). Also, notice that you don't have to include the "x:y" part at the end of the device name. In fact, it's probably better to leave this part out since it can (and probably will) change between reboots of the computer.
 Respond_to_midi_channels = [15] # List which midi channels should be listened to for midi messages. Notice that counting starts at zero, meaning that what most devices call midi channel 1 will be represented by 0 in this array.
 Clock_ticks_per_cycle = 2 # How many ticks (one quarter note consists of 24 ticks) one cycle of the program should consist off. Lower values means lower latency, but if the value is set to low, cycles might become uneven in length.
+DMX_driver = "FT232R" # Can be replaced with "Dummy" for testing without a connected interface. Use "FT232R" for now.
 
 Max_brightness = 127 # In DMX value. So the minumum is 0 and the maximum is 255.
 Max_Attack_cycles = 127 # If multiplied with Clock_ticks_per_cycle above, this results in the maximum number of ticks the attack phase can be.
@@ -262,7 +263,7 @@ class Layer2:
 ### Main program ###
 ####################
 ### Open an interface
-with DMXInterface("FT232R") as interface:
+with DMXInterface(DMX_driver) as interface:
 
     Layer0 = Layer0(Number_of_lights = Number_of_lights) # Initialize a Layer0.
     Layer1 = Layer1(Number_of_lights = Number_of_lights) # Initialize a Layer1.
